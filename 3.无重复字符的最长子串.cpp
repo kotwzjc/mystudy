d75acb16657d0973b0ecb90356b37cf616b55eda
kotwzjc <kotwzjc@whu.edu.cn>
2021-03-20 21:07:31 +0800

    第三题

---
+++ 3.无重复字符的最长子串.cpp
@@ -0,0 +1,126 @@
+/*
+ * @Author: kotwzjc
+ * @Github: https://github.com/kotwzjc
+ * @Date: 2021-03-15 20:50:18
+ * @LastEditors: kotwzjc
+ * @LastEditTime: 2021-03-20 21:03:51
+ */
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ *
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
+ *
+ * algorithms
+ * Medium (36.63%)
+ * Likes:    5122
+ * Dislikes: 0
+ * Total Accepted:    879.1K
+ * Total Submissions: 2.4M
+ * Testcase Example:  '"abcabcbb"'
+ *
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: s = "abcabcbb"
+ * 输出: 3 
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 
+ * 输入: s = ""
+ * 输出: 0
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * s 由英文字母、数字、符号和空格组成
+ * 
+ * 
+ */
+
+// @lc code=start
+/*
+// my solution
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> hashTable;
+        int l = s.size();
+        int result = 0, tempResult = 0;
+        int i = 0, lastStart = 0;
+        for(; i < l; i++){
+            if(hashTable.find(s[i]) != hashTable.end()){
+                int nextStart = hashTable[s[i]] + 1;
+                result = result < tempResult?tempResult:result;
+                for(int k = lastStart; k < nextStart; k++)
+                    hashTable.erase(s[k]);
+                lastStart = nextStart;
+                i--;
+                tempResult = i - lastStart + 1;
+            }
+            else{
+                tempResult++;
+                hashTable.insert(pair<char, int>(s[i], i));
+            }
+        }
+        result = result < tempResult?tempResult:result;
+        return result;
+    }
+};*/
+// recommend solution
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // 哈希集合，记录每个字符是否出现过
+        unordered_set<char> occ;
+        int n = s.size();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        // 枚举左指针的位置，初始值隐性地表示为 -1
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.erase(s[i - 1]);
+            }
+            while (rk + 1 < n && !occ.count(s[rk + 1])) {
+                // 不断地移动右指针
+                occ.insert(s[rk + 1]);
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
